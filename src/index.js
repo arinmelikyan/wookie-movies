2ba4aba9d0957b4398ddb573cffb33081c26bedc
Arin Melikyan <arin.melkn@gmail.com>
2023-03-03 04:48:05 +0400

    switch to strict mode

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ const history = createBrowserHistory();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.Fragment>
+  <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter history={history}>
         <Routes />
       </BrowserRouter>
     </Provider>
-  </React.Fragment>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
